5c05349446f55c5703e89811d24aea2263530f9e
Asaf34554 <Asaf34554@gmail.com>
2024-01-30 23:27:21 +0200

    almost done

--- shell.h
+++ shell.h
@@ -1,12 +1,22 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+typedef struct _VAR{
+    char vname [256];
+    char  vvalue[256];
+}vari;
+
+
+void redct(char**,char*,int*,int*);
+void if_then_else(char*,char*,int*,int*,const int);
+void add_var(char*,vari*,int*);
+char* get_vval(char*,vari*,const int);
 void cd_dir(char*[],int);
 void sig_int(int);
 void divide_cmd(char**,int*,char*,const char*);
 void unspace(char*);
 void pipe_hndl(char **, int );
-
+void read_var(char**,vari*,int*,const int);
 
 
 #endif
--- shell2.c
+++ shell2.c
@@ -1,21 +1,93 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-#include "stdio.h"
+#include "stdio.h" // Standard I/O functions
 #include "errno.h"
 #include "stdlib.h"
-#include "unistd.h"
+#include "unistd.h" // Symbolic Constants and Types, Function Declarations
 #include <string.h>
 #include "shell.h"
 #include <signal.h>
 
-
-
 #define MAX_COMMAND_SIZE 1024
 
+void redct(char** argv,char *outfile,int* i,int* redirect){
+    if (!strcmp(argv[*i - 2], ">") || !strcmp(argv[*i - 2], "2>") || !strcmp(argv[*i - 2], ">>")) {
+        if (!strcmp(argv[*i - 2], ">"))
+            *redirect = 1;
+
+        else if(! strcmp(argv[*i - 2], "2>"))
+            *redirect = 2;
+
+        else // ">>"
+            *redirect = 3;
+
+        argv[*i - 2] = NULL;
+        strcpy(outfile,argv[*i-1]);
+        *i = *i - 2;
+    }
+    else
+        *redirect = 0;
+
+}
+
+void if_then_else(char *comm,char *outcmd,int *t_flag,int *e_flag,const int len){
+    /* Function to handle if-then-else-fi conditional statements */
+
+    // Extract data
+    char cmd[MAX_COMMAND_SIZE] = "\0";
+    char tmp[MAX_COMMAND_SIZE] = "\0";
+    strcpy(tmp,comm);
+    int lens = 0;
+    char *argv[10];
+    divide_cmd(argv,&lens,tmp," ");
+    char then_cmd[MAX_COMMAND_SIZE] = "\0",do_cmd[MAX_COMMAND_SIZE] = "\0",else_cmd[MAX_COMMAND_SIZE] = "\0";
+    char else_do_cmd[MAX_COMMAND_SIZE] = "\0",fi_cmd[MAX_COMMAND_SIZE] = "\0";
+
+    // Build the command to run in system and check if it's valid
+    for (int i = 1; i < lens; ++i)
+    {
+        strcat(cmd, argv[i]);
+        strcat(cmd, " ");
+    }
+    fgets(then_cmd, MAX_COMMAND_SIZE, stdin); // Read 'then' command
+    fgets(do_cmd, MAX_COMMAND_SIZE, stdin); // Read 'do' command
+
+    fgets(else_cmd, MAX_COMMAND_SIZE, stdin); // Read 'else' command
+
+    if(!strcmp(else_cmd,"else\n")){
+        fgets(else_do_cmd, MAX_COMMAND_SIZE, stdin); // Read 'else-do' command
+        fgets(fi_cmd, MAX_COMMAND_SIZE, stdin); // Read 'fi' command
+    }
+    else if(!strcmp(else_cmd,"fi\n")){
+        strcpy(fi_cmd,else_cmd); // Copy 'fi' command
+    }
+    
+    if (!strcmp(then_cmd, "then\n") && !strcmp(fi_cmd, "fi\n"))
+    {
+        if (!system(cmd))
+        {
+            // Execute 'do' command
+            *t_flag = 1;
+            strcpy(outcmd,do_cmd);
+        }
+        else
+        {
+            // Execute 'else-do' command
+            *e_flag = 1;
+            strcpy(outcmd,else_do_cmd);
+        }
+    }
+    else
+    {
+        printf("Bad syntax in if-then-else-fi, enter again\n");
+    }
+}
 
 void divide_cmd(char **cmdbuffer, int *n, char *cmd, const char* ch)
 {
+    /* Function to divide command into tokens */
+
     char *tkn;
     tkn = strtok(cmd, ch);
     int ndex = -1;
@@ -23,14 +95,16 @@ void divide_cmd(char **cmdbuffer, int *n, char *cmd, const char* ch)
     {
         cmdbuffer[++ndex] = malloc(sizeof(tkn) + 1);
         strcpy(cmdbuffer[ndex], tkn);
-        unspace(cmdbuffer[ndex]);
+        unspace(cmdbuffer[ndex]); // Remove leading and trailing spaces
         tkn = strtok(NULL, ch);
     }
     cmdbuffer[++ndex] = NULL;
-    *n = ndex;
+    (*n) = ndex;
 }
 
 void unspace(char* cmd){
+    /* Function to remove leading and trailing spaces from a string */
+
     if(cmd[0] == ' ' || cmd[0] == '\n'){
         memmove(cmd,cmd+1,strlen(cmd));
     }
@@ -40,46 +114,78 @@ void unspace(char* cmd){
 }
 
 void pipe_hndl(char **cmdbuf, int index){
+    /* Function to handle pipe commands */
+    char outfile[1024] = "\0";
     if (index > 50)
     {
         return;
     }
 
     int fd[50][2]; // 50 pipes
-    int indx;
+    int indx =0,red=0;
     char *cmd[50];
+    
 
     for (int i = 0; i < index; i++)
     {
+        
         divide_cmd(cmd, &indx, cmdbuf[i], " ");
-
+        redct(cmd,outfile,&indx,&red);
+        printf("%s\n",cmd[i]);
         if (i != index - 1){
-
             if (pipe(fd[i]) == -1){
                 perror("ERROR IN PIPE!\n");
                 return;
             }
         }
-        /*child*/
+
+        /* Child process */
         if (fork() == 0)
         {
+            signal(SIGINT,SIG_DFL);
             if (i != index - 1)
             {
                 dup2(fd[i][1], STDOUT_FILENO);
                 close(fd[i][0]);
                 close(fd[i][1]);
             }
-
-            if (i != 0)
+            
+            if(i == index-1 && red != 0){
+                int tfd;
+                if (red == 1) { // ">"
+                    tfd = creat(outfile, 0660); 
+                    dup2(tfd, STDOUT_FILENO);
+                    close(tfd);
+                    /* stdout is now redirected */
+                }
+                else if(red == 2){ // "2>"
+                    tfd = creat(outfile, 0660); 
+                    dup2(tfd, STDERR_FILENO); 
+                    close(tfd); 
+                    /* stderr is now redirected */
+                }
+                else if (red == 3){ // ">>"
+                    tfd = open(outfile, O_WRONLY | O_APPEND | O_CREAT, 0660);
+                    dup2(tfd, STDOUT_FILENO); 
+                    close(tfd); 
+                    /* stdout is now redirected */
+                }
+            }
+            if(i != 0)
             {
                 dup2(fd[i - 1][0], STDIN_FILENO);
                 close(fd[i - 1][1]);
                 close(fd[i - 1][0]);
             }
+            
+            
+
+            /* redirection of IO ? */
+
             execvp(cmd[0], cmd);
             exit(1);
         }
-        /*parent*/
+        /* Parent process */
         if (i != 0)
         {
             close(fd[i - 1][0]);
@@ -92,8 +198,66 @@ void pipe_hndl(char **cmdbuf, int index){
         }
     }
 }
+
+
+
+void read_var(char** argv,vari *varbuff,int *varcount,const int len){
+    /* Function to read variable values */
+
+    int idx=0;
+    char rcmd[MAX_COMMAND_SIZE]="\0";
+    char* rargv[10];
+    fgets(rcmd,MAX_COMMAND_SIZE,stdin);
+    divide_cmd(rargv,&idx,rcmd," ");
+    for(int i=1;i<len;i++){
+        sprintf(varbuff[*varcount].vname,"$%s",argv[i]);
+        if(i-1 < idx){
+            strcpy(varbuff[*varcount].vvalue,rargv[i-1]);
+        }
+        else
+        {
+            strcpy(varbuff[*varcount].vvalue," ");
+        }
+        (*varcount)++;
+    } 
+}
+
+char* get_vval(char* vname,vari* vars, const int numv){
+    /* Function to get the value of a variable */
+
+    for(int i=0;i<numv;i++){
+        if(!strcmp(vars[i].vname,vname)){
+            return vars[i].vvalue;
+        }
+    }
+    return NULL;
+}
+
+void add_var(char *cmd,vari *vbuff,int *varcount){
+    /* Function to add a variable */
+
+    int index = 0;
+    char *token = strtok(cmd," ");
+    while (token != NULL)
+    {
+        if(index == 0){
+            strcpy(vbuff[*varcount].vname,token);
+                  
+        }
+        if(index == 2)
+        {
+            strcpy(vbuff[*varcount].vvalue,token); 
+        }
+        token = strtok(NULL," ");
+        ++index;
+
+    }
+    (*varcount)++;
+}
+
 void cd_dir(char* argv [],int len){
-    // char* ans;
+    /* Function to handle cd command */
+
     if(len == 1 || !strcmp(argv[1],"~")){
         const char *home_dir = getenv("HOME");
         if(home_dir == NULL)
@@ -109,7 +273,7 @@ void cd_dir(char* argv [],int len){
         }
     }
     else{
-        char dir[1024] = "";
+        char dir[MAX_COMMAND_SIZE] = "";
         for(int i = 1;i<len;i++){
             strcat(dir,argv[i]);
             if(i < len -1)
@@ -124,42 +288,55 @@ void cd_dir(char* argv [],int len){
             }
         else{
             printf("too many arguments in cd, try using quotes signs on the folder name\n");
-            // perror("cd:");
         }
     }
 
 }
 
 void sig_int(int sig){
+    /* Signal handler for SIGINT (CTRL+C) */
+
     printf("\nYou typed Control-C!\n");
 }
 
-
-
 int main() {
+    /* Main function */
+
+    char then_else_cmd[MAX_COMMAND_SIZE];
     char command[MAX_COMMAND_SIZE];
     char last_cmd[MAX_COMMAND_SIZE];
     char pipe_cmd[MAX_COMMAND_SIZE];
     char *token;
-    char outfile[1024];
-    int i, fd, amper, redirect, retid, status,id_len;
+    char outfile[MAX_COMMAND_SIZE];
+    int i, fd, amper, redirect, retid, status,pipecount=0,varcount=0,t_flag = 0,e_flag=0;
     char *argv[10];
     char *cmdbuffer[256];
-
+    vari varbuff[20];
     char prompt[256];
-    strcpy(prompt,"Maccabi Haifa");
-
-
+    strcpy(prompt,"hello");
 
     // Handles CTRL+C Signal and ignores it
     signal(SIGINT,sig_int);
+
     while (1)
     {   
-        printf("%s: ",prompt);    
-        fgets(command, MAX_COMMAND_SIZE, stdin);
-        command[strlen(command) - 1] = '\0';
-        
-
+        if(t_flag){
+            strcpy(command,then_else_cmd);
+            command[strlen(command) - 1] = '\0'; /* code */
+            t_flag = 0;
+        }
+        else if(e_flag){
+            strcpy(command,then_else_cmd);
+            command[strlen(command) - 1] = '\0'; /* code */
+            e_flag = 0;
+        }
+        else
+        {
+            printf("%s: ",prompt);    
+            fgets(command, MAX_COMMAND_SIZE, stdin);
+            command[strlen(command) - 1] = '\0'; /* code */
+        }
+ 
         if(strncmp(command,"!!",2) ){
             strcpy(last_cmd,command);
         }
@@ -167,6 +344,13 @@ int main() {
         if(! strcmp(command,"quit")){
             exit(0);
         }
+
+        if (!strncmp(command,"$",1))
+        {
+            add_var(command,varbuff,&varcount);
+            continue;
+        }
+        
 
         /* parse command line */ 
         strcpy(pipe_cmd,command);
@@ -179,8 +363,8 @@ int main() {
             i++;
             if (token && !strcmp(token, "|")){ 
                 
-                divide_cmd(cmdbuffer, &id_len, pipe_cmd, "|");
-                pipe_hndl(cmdbuffer, id_len);
+                divide_cmd(cmdbuffer, &pipecount, pipe_cmd, "|");
+                pipe_hndl(cmdbuffer, pipecount);
                 break;
             }
         }
@@ -235,28 +419,20 @@ int main() {
             i = j;  
         }
         if(i > 2){
-            if (!strcmp(argv[i - 2], ">") || !strcmp(argv[i - 2], "2>") || !strcmp(argv[i - 2], ">>")) {
-                if (!strcmp(argv[i - 2], ">"))
-                    redirect = 1;
-
-                else if(! strcmp(argv[i - 2], "2>"))
-                    redirect = 2;
-
-                else // ">>"
-                    redirect = 3;
-                
-                argv[i - 2] = NULL;
-                strcpy(outfile,argv[i-1]);
-            } 
-            else
-                redirect = 0;
+            redct(argv,outfile,&i,&redirect);
         }
         else
             redirect = 0;
 
 
+        /*Check if the command is if and else*/
+        if (!strcmp(argv[0], "if")){
+            if_then_else(last_cmd,then_else_cmd,&t_flag,&e_flag,i);
+            continue;
+        }
+        
         /* Check if the command is for changing the prompt */
-        if (strcmp(argv[0], "prompt") == 0 && strcmp(argv[1], "=") == 0 && i == 3) {
+        if (strcmp(argv[0], "prompt") == 0 &&  i == 3 && strcmp(argv[1], "=") == 0) {
             strcpy(prompt,argv[2]);
             continue;
         }
@@ -265,9 +441,23 @@ int main() {
             continue;
         }
 
-        //$? command
-        if( argv[1] != NULL && !( strcmp(argv[1], "$?"))){        
-            strcpy(argv[1],strerror(status));
+        //read command
+        if(!strcmp(argv[0],"read")){
+            read_var(argv,varbuff,&varcount,i);
+            continue;
+        }
+
+        //echo command
+        if( argv[0] != NULL && ! strcmp(argv[0], "echo")){  
+            for (int idx = 1; idx < i; idx++)
+            {
+                if(!strcmp(argv[idx],"$?"))   {   
+                    sprintf(argv[idx],"%d",status);
+                }
+                else if (argv[idx][0] == '$'){
+                    argv[idx]=get_vval(argv[idx],varbuff,varcount);
+                }
+            }                      
         }
 
         //cd command
@@ -301,7 +491,6 @@ int main() {
                 dup(fd); 
                 close(fd); 
                 /* stdout is now redirected */
-
             }
 
             int statusc = execvp(argv[0], argv);
